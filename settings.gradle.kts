b5ce277abe93f7f341a8051cb05d63910cd45f6c
임세현 <sh00710000@gmail.com>
2022-06-14 22:51:01 +0900

    feat: init project

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "AndroidArchitectureSample"
+include (":app")
+include(":core-navigator")
+include(":feature-notice")
+include(":common-design")
+include(":feature-auth")
+include(":core-domain")
+include(":core-data")
+include(":core-remote")
+include(":core-local")
+include(":common-di")
