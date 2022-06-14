fc87794e1be16508a6b89577b051c438acb44c88
임세현 <sh00710000@gmail.com>
2022-06-14 23:18:19 +0900

    feat: app module

---
+++ app/src/main/java/com/saehyun/androidarchitecture/di/NavigationModule.kt
@@ -0,0 +1,4 @@
+package com.saehyun.androidarchitecture.di
+
+interface NavigationModule {
+}
---
+++ app/src/main/java/com/saehyun/androidarchitecture/root/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.saehyun.androidarchitecture.root
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.saehyun.core_navigator.SampleRoute
+import com.saehyun.feature_auth.navigation.authNavigation
+import com.saehyun.feature_notice.navigation.noticeNavigation
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        installSplashScreen()
+
+        setContent {
+            val navController = rememberNavController()
+
+            NavHost(
+                navController = navController,
+                startDestination = SampleRoute.Auth.name
+            ) {
+                authNavigation(
+                    composeNavigator = navController
+                )
+
+                noticeNavigation(
+                    composeNavigator = navController
+                )
+            }
+        }
+    }
+}
