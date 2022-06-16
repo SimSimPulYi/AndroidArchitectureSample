e79b674b36525868d93f2d14270c8cef25dc74eb
임세현 <sh00710000@gmail.com>
2022-06-16 22:13:58 +0900

    feat: notice api

---
+++ core-data/src/main/java/com/saehyun/core_data/remote/api/NoticeAPI.kt
@@ -0,0 +1,10 @@
+package com.saehyun.core_data.remote.api
+
+import com.saehyun.core_data.remote.response.PostResponse
+import kotlinx.coroutines.flow.Flow
+import retrofit2.http.GET
+
+interface NoticeAPI {
+    @GET("posts")
+    suspend fun fetchPosts(): Flow<PostResponse>
+}
