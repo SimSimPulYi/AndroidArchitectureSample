88338d937aa50ee8e8e1293012dcb957b4e0b82d
임세현 <sh00710000@gmail.com>
2022-06-17 23:04:06 +0900

    feat: design resource

--- feature-notice/src/main/java/com/saehyun/feature_notice/model/NoticeModel.kt
+++
@@ -1,6 +0,0 @@
-package com.saehyun.feature_notice.model
-
-data class NoticeModel(
-    val title: String,
-    val content: String
-)
--- feature-notice/src/main/java/com/saehyun/feature_notice/state/NoticeState.kt
+++
@@ -1,9 +0,0 @@
-package com.saehyun.feature_notice.state
-
-sealed class NoticeState {
-    object Loading : NoticeState()
-
-    data class Success(val noticeState: NoticeState) : NoticeState()
-
-    object Failure : NoticeState()
-}
