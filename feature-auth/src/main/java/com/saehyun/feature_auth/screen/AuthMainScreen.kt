d03139dea301f4b9fa7835163595b0ef95d426f4
임세현 <sh00710000@gmail.com>
2022-06-17 23:07:18 +0900

    feat: auth main screen

--- feature-auth/src/main/java/com/saehyun/feature_auth/screen/AuthMainScreen.kt
+++ feature-auth/src/main/java/com/saehyun/feature_auth/screen/AuthMainScreen.kt
@@ -1,32 +1,66 @@
 package com.saehyun.feature_auth.screen
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.saehyun.common_design.component.RedButton
+import com.saehyun.common_base.observeWithLifecycle
+import com.saehyun.common_design.component.SampleTextField
+import com.saehyun.common_design.component.YellowButton
+import com.saehyun.common_design.theme.Body1
 import com.saehyun.common_design.theme.SampleTheme
 import com.saehyun.core_navigator.SampleScreen
+import com.saehyun.feature_auth.mvi.AuthMainSideEffect
+import com.saehyun.feature_auth.vm.AuthMainVM
+import kotlinx.coroutines.InternalCoroutinesApi
 
+@OptIn(InternalCoroutinesApi::class)
 @Composable
 fun AuthMainScreen(
-    composeNavigator: NavController
+    composeNavigator: NavController,
+    authMainVM: AuthMainVM
 ) {
-    NavController
-    SampleTheme(darkTheme = true) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            RedButton(text = "Next Step") {
+    val authMainContainer = authMainVM.container
+    val authMainState = authMainContainer.stateFlow.collectAsState().value
+    val authMainSideEffect = authMainContainer.sideEffectFlow
+
+    authMainSideEffect.observeWithLifecycle {
+        when(it) {
+            is AuthMainSideEffect.IntentNotice -> {
                 composeNavigator.navigate(SampleScreen.Notice.NOTICE_MAIN) {
                     launchSingleTop = true
                 }
             }
+        }
+    }
+
+    SampleTheme(darkTheme = true) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(120.dp))
+
+            Body1(text = "로그인")
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            SampleTextField(value = authMainState.username, onValueChange = { authMainVM.inputUsername(it)} )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            SampleTextField(value = authMainState.password, onValueChange = { authMainVM.inputPassword(it) } )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            YellowButton(text = "LOGIN", enabled = authMainState.loginBtnEnabled, onClick = { authMainVM.intentAuth() })
         }
     }
 }
@@ -34,5 +68,5 @@ fun AuthMainScreen(
 @Preview
 @Composable
 fun PreviewAuthMainScreen() {
-    AuthMainScreen(rememberNavController())
+    AuthMainScreen(rememberNavController(), AuthMainVM())
 }
