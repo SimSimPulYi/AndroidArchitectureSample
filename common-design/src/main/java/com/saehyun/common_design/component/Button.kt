8bbfd0b9586c36198d99fc8b8354e094472afd7c
임세현 <sh00710000@gmail.com>
2022-06-17 23:07:57 +0900

    feat: design component
    sample textfield & next step button

--- common-design/src/main/java/com/saehyun/common_design/component/Button.kt
+++ common-design/src/main/java/com/saehyun/common_design/component/Button.kt
@@ -3,18 +3,21 @@ package com.saehyun.common_design.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.saehyun.common_design.theme.Body1
-import com.saehyun.common_design.theme.primaryBlue
+import com.saehyun.common_design.theme.HalfOrange
+import com.saehyun.common_design.theme.PrimaryOrange
+import com.saehyun.common_design.theme.SampleTypography
 
 @Composable
 fun RedButton(
@@ -25,7 +28,7 @@ fun RedButton(
         modifier = Modifier
             .height(20.dp)
             .background(
-                color = primaryBlue,
+                color = PrimaryOrange,
                 shape = RoundedCornerShape(16.dp)
             )
             .padding(horizontal = 12.dp)
@@ -35,6 +38,30 @@ fun RedButton(
         contentAlignment = Alignment.Center
     ) {
         Body1(text = text, modifier = Modifier.padding(vertical = 6.dp))
+    }
+}
+
+@Composable
+fun YellowButton(
+    text: String,
+    enabled: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .height(50.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(5.dp))
+            .background(if (enabled) PrimaryOrange else HalfOrange)
+            .clickable(
+                onClick = {
+                    if (enabled) onClick()
+                }
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = text, style = SampleTypography.body1)
     }
 }
 
