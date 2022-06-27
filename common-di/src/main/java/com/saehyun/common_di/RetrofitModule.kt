28e5f69617e0ff27db416d63df3693f7317bab68
임세현 <sh00710000@gmail.com>
2022-06-27 21:50:09 +0900

    feat: retrofit module

---
+++ common-di/src/main/java/com/saehyun/common_di/RetrofitModule.kt
@@ -0,0 +1,46 @@
+package com.saehyun.common_di
+
+import android.util.Log
+import com.saehyun.core_data.remote.api.NoticeAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    private val BASE_URL = ""
+
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor { message -> Log.v("HTTP", message) }
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Provides
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    fun provideSchoolApi(retrofit: Retrofit): NoticeAPI =
+        retrofit.create(NoticeAPI::class.java)
+}
