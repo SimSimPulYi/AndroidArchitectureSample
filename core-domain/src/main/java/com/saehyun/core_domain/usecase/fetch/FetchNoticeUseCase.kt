01d947b78a996fdbf575a7723aff496d62db26f5
임세현 <sh00710000@gmail.com>
2022-07-01 16:32:40 +0900

    feat: notice usecase

--- core-domain/src/main/java/com/saehyun/core_domain/usecase/fetch/FetchNoticeUseCase.kt
+++ core-domain/src/main/java/com/saehyun/core_domain/usecase/fetch/FetchNoticeUseCase.kt
@@ -1,16 +1,14 @@
 package com.saehyun.core_domain.usecase.fetch
 
-import android.content.Context
 import com.saehyun.core_domain.model.FetchNoticeEntity
 import com.saehyun.core_domain.repository.NoticeRepository
 import com.saehyun.core_domain.usecase.BaseUseCase
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class FetchNoticeUseCase @Inject constructor(
     private val noticeRepository: NoticeRepository
-): BaseUseCase<Result<FetchNoticeEntity>, Unit> {
-    override suspend fun perform(): Result<FetchNoticeEntity> =
+): BaseUseCase<Result<List<FetchNoticeEntity>>, Unit> {
+    override suspend fun perform(): Result<List<FetchNoticeEntity>> =
         kotlin.runCatching {
             noticeRepository.fetchNotice()
         }
