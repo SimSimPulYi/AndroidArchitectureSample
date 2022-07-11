aa419cfb0df1fa30728a15839cbdd15ba7000560
임세현 <sh00710000@gmail.com>
2022-07-11 15:59:14 +0900

    fix: custom result 사용하여 대응

--- feature-notice/src/main/java/com/saehyun/feature_notice/vm/NoticeMainVM.kt
+++ feature-notice/src/main/java/com/saehyun/feature_notice/vm/NoticeMainVM.kt
@@ -1,10 +1,15 @@
 package com.saehyun.feature_notice.vm
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.saehyun.core_domain.base.Result
+import com.saehyun.core_domain.exception.NoInternetException
+import com.saehyun.core_domain.exception.UnknownException
 import com.saehyun.core_domain.usecase.fetch.FetchNoticeUseCase
 import com.saehyun.feature_notice.mvi.NoticeMainSideEffect
 import com.saehyun.feature_notice.mvi.NoticeMainState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.postSideEffect
@@ -15,27 +20,47 @@ import javax.inject.Inject
 @HiltViewModel
 class NoticeMainVM @Inject constructor(
     private val fetchNoticeUseCase: FetchNoticeUseCase
-): ContainerHost<NoticeMainState, NoticeMainSideEffect>, ViewModel() {
+) : ContainerHost<NoticeMainState, NoticeMainSideEffect>, ViewModel() {
 
     override val container = container<NoticeMainState, NoticeMainSideEffect>(NoticeMainState())
 
-     fun fetchPost() = intent {
+    fun fetchPost() = intent {
         loading(true)
-        fetchNoticeUseCase.perform()
-            .onSuccess {
-                reduce { state.copy(notices = it) }
-                postSideEffect(NoticeMainSideEffect.FetchNoticeSuccess)
-            }
-            .onFailure {
-                postSideEffect(NoticeMainSideEffect.FetchNoticeFailed(message = "error!"))
-            }
-            .also {
-                loading(false)
-            }
+
+        fetchNoticeUseCase()
+            .onEach { result ->
+                when (result) {
+                    is Result.Loading -> {
+                        loading(false)
+                    }
+
+                    is Result.Success -> {
+                        reduce { state.copy(notices = result.value) }
+                        postSideEffect(NoticeMainSideEffect.FetchNoticeSuccess)
+                    }
+
+                    is Result.Failure -> {
+                        result.throwable.let {
+                            when (it) {
+                                is NoInternetException -> {
+                                    failed(it.message)
+                                }
+                                is UnknownException -> {
+                                    failed(it.message)
+                                }
+                            }
+                        }
+                    }
+                }
+            }.launchIn(viewModelScope)
     }
 
     fun changeColorTheme() = intent {
         reduce { state.copy(darkTheme = !state.darkTheme) }
+    }
+
+    private fun failed(message: String) = intent {
+        postSideEffect(NoticeMainSideEffect.FetchNoticeFailed(message = message))
     }
 
     private fun loading(isLoading: Boolean) = intent {
