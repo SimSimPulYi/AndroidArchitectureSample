564e6569916291677ec127ed8e00a1aaf9e64466
임세현 <sh00710000@gmail.com>
2022-07-11 15:58:53 +0900

    fix: custom result에 맞게 emit

--- core-domain/src/main/java/com/saehyun/core_domain/usecase/fetch/FetchNoticeUseCase.kt
+++ core-domain/src/main/java/com/saehyun/core_domain/usecase/fetch/FetchNoticeUseCase.kt
@@ -1,15 +1,31 @@
 package com.saehyun.core_domain.usecase.fetch
 
-import com.saehyun.core_domain.model.FetchNoticeEntity
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.saehyun.core_domain.base.Result
 import com.saehyun.core_domain.repository.NoticeRepository
-import com.saehyun.core_domain.usecase.BaseUseCase
+import com.saehyun.core_domain.base.BaseUseCase
+import com.saehyun.core_domain.model.FetchNoticeEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import java.lang.Exception
 import javax.inject.Inject
 
 class FetchNoticeUseCase @Inject constructor(
     private val noticeRepository: NoticeRepository
-): BaseUseCase<Result<List<FetchNoticeEntity>>, Unit> {
-    override suspend fun perform(): Result<List<FetchNoticeEntity>> =
-        kotlin.runCatching {
-            noticeRepository.fetchNotice()
+): BaseUseCase<List<FetchNoticeEntity>, Unit> {
+
+    override operator fun invoke() = flow {
+
+        try {
+            noticeRepository.fetchNotice().collect {
+                emit(Result.Success(it))
+            }
+        } catch (e: Exception) {
+            emit(Result.Failure(e))
         }
+
+        emit(Result.Loading)
+
+    }.flowOn(Dispatchers.IO)
 }
