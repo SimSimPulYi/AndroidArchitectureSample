0fe9aacc417f812b2c509e149f609de64e0f976c
임세현 <sh00710000@gmail.com>
2022-07-11 15:56:03 +0900

    fix: fetch notice에 offline caching 적용

--- core-domain/src/main/java/com/saehyun/core_domain/repository/NoticeRepository.kt
+++ core-domain/src/main/java/com/saehyun/core_domain/repository/NoticeRepository.kt
@@ -1,7 +1,8 @@
 package com.saehyun.core_domain.repository
 
 import com.saehyun.core_domain.model.FetchNoticeEntity
+import kotlinx.coroutines.flow.Flow
 
 interface NoticeRepository {
-    suspend fun fetchNotice(): List<FetchNoticeEntity>
+    suspend fun fetchNotice(): Flow<List<FetchNoticeEntity>>
 }
