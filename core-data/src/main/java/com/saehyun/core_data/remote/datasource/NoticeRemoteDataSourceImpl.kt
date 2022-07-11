8126c7d62b1920dc4075f14f1802138fab3bd125
임세현 <sh00710000@gmail.com>
2022-07-11 15:55:19 +0900

    fix: remote에서 발생하는 오류 try catch

--- core-data/src/main/java/com/saehyun/core_data/remote/datasource/NoticeRemoteDataSourceImpl.kt
+++ core-data/src/main/java/com/saehyun/core_data/remote/datasource/NoticeRemoteDataSourceImpl.kt
@@ -2,13 +2,21 @@ package com.saehyun.core_data.remote.datasource
 
 import com.saehyun.core_data.remote.api.NoticeAPI
 import com.saehyun.core_data.remote.response.PostResponse
+import com.saehyun.core_domain.exception.NoInternetException
+import com.saehyun.core_domain.exception.UnknownException
 import kotlinx.coroutines.flow.Flow
+import java.lang.Exception
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 class NoticeRemoteDataSourceImpl @Inject constructor(
     private val noticeAPI: NoticeAPI
 ): NoticeRemoteDataSource  {
     override suspend fun fetchPosts(): List<PostResponse> {
-        return noticeAPI.fetchPosts()
+        return try {
+            noticeAPI.fetchPosts()
+        } catch (e: UnknownHostException) {
+            throw NoInternetException()
+        }
     }
 }
